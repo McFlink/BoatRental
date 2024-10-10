5af4a1b5be7a87b61cbb65db864819f72337d8c9
McFlink <mikaelcflink@gmail.com>
2024-10-10 22:20:26 +0200

    Add project files.

---
+++ Data/ApplicationUser.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace BoatRental.Data
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+
+        [Required]
+        public override string? PhoneNumber { get; set; } // Required annotation but nullable string because has to follow base implementation of Identity's PhoneNumber column which is nullable. This will make the PhoneNumber field to be required.
+    }
+}
