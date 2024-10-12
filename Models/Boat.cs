fe357ab23d5022036780e26e5170c94a805507af
McFlink <mikaelcflink@gmail.com>
2024-10-12 16:17:48 +0200

    Minor style changes. Finished (for now with basics)

--- Models/Boat.cs
+++ Models/Boat.cs
@@ -7,8 +7,8 @@ namespace BoatRental.Models
         public int Id { get; set; }
         public required string Description { get; set; }
         public required decimal PricePerDay { get; set; }
-        public required string Location { get; set; } // Coordinates
-        public required string PostalCode { get; set; }
+        public string? Location { get; set; } // Coordinates
+        public string? PostalCode { get; set; }
         public int? LockCombination { get; set; }
         public int UserId { get; set; }
         public bool IsAvailable { get; set; } = true;
